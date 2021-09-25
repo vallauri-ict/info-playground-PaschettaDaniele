b9becce768bbdd44cbef78b278d3cc8420208ba2
Daniele Paschetta <d.paschetta.1775@vallauri.edu>
2021-09-25 10:46:06 +0200

    es2 - MDIA - Menu - Barra di stato

---
+++ 02 - WindowsForms/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace _02___WindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFrm1 = new System.Windows.Forms.Button();
+            this.btnFrm2 = new System.Windows.Forms.Button();
+            this.btnFinestreAperte = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menùToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.apriForm1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.apriForm2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.esciToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnFrm1
+            // 
+            this.btnFrm1.Location = new System.Drawing.Point(12, 27);
+            this.btnFrm1.Name = "btnFrm1";
+            this.btnFrm1.Size = new System.Drawing.Size(210, 49);
+            this.btnFrm1.TabIndex = 1;
+            this.btnFrm1.Text = "APRI FORM 1";
+            this.btnFrm1.UseVisualStyleBackColor = true;
+            this.btnFrm1.Click += new System.EventHandler(this.btnFrm1_Click);
+            // 
+            // btnFrm2
+            // 
+            this.btnFrm2.Location = new System.Drawing.Point(228, 27);
+            this.btnFrm2.Name = "btnFrm2";
+            this.btnFrm2.Size = new System.Drawing.Size(210, 49);
+            this.btnFrm2.TabIndex = 2;
+            this.btnFrm2.Text = "APRI FORM 2";
+            this.btnFrm2.UseVisualStyleBackColor = true;
+            this.btnFrm2.Click += new System.EventHandler(this.btnFrm2_Click);
+            // 
+            // btnFinestreAperte
+            // 
+            this.btnFinestreAperte.Location = new System.Drawing.Point(13, 82);
+            this.btnFinestreAperte.Name = "btnFinestreAperte";
+            this.btnFinestreAperte.Size = new System.Drawing.Size(425, 49);
+            this.btnFinestreAperte.TabIndex = 3;
+            this.btnFinestreAperte.Text = "FINESTRE APERTE";
+            this.btnFinestreAperte.UseVisualStyleBackColor = true;
+            this.btnFinestreAperte.Click += new System.EventHandler(this.btnFinestreAperte_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menùToolStripMenuItem,
+            this.infoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(450, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menùToolStripMenuItem
+            // 
+            this.menùToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.apriForm1ToolStripMenuItem,
+            this.apriForm2ToolStripMenuItem,
+            this.esciToolStripMenuItem});
+            this.menùToolStripMenuItem.Name = "menùToolStripMenuItem";
+            this.menùToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menùToolStripMenuItem.Text = "&Menù";
+            // 
+            // apriForm1ToolStripMenuItem
+            // 
+            this.apriForm1ToolStripMenuItem.Name = "apriForm1ToolStripMenuItem";
+            this.apriForm1ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.apriForm1ToolStripMenuItem.Text = "Apri Form &1";
+            this.apriForm1ToolStripMenuItem.Click += new System.EventHandler(this.apriForm1ToolStripMenuItem_Click);
+            // 
+            // apriForm2ToolStripMenuItem
+            // 
+            this.apriForm2ToolStripMenuItem.Name = "apriForm2ToolStripMenuItem";
+            this.apriForm2ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.apriForm2ToolStripMenuItem.Text = "Apri Form &2";
+            this.apriForm2ToolStripMenuItem.Click += new System.EventHandler(this.apriForm2ToolStripMenuItem_Click);
+            // 
+            // esciToolStripMenuItem
+            // 
+            this.esciToolStripMenuItem.Name = "esciToolStripMenuItem";
+            this.esciToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.esciToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.esciToolStripMenuItem.Text = "Esci";
+            this.esciToolStripMenuItem.Click += new System.EventHandler(this.esciToolStripMenuItem_Click);
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2});
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.infoToolStripMenuItem.Text = "In&fo";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(79, 22);
+            this.toolStripMenuItem2.Text = "?";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 370);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(450, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(64, 17);
+            this.toolStripStatusLabel.Text = "Benvenuto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 392);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnFinestreAperte);
+            this.Controls.Add(this.btnFrm2);
+            this.Controls.Add(this.btnFrm1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFrm1;
+        private System.Windows.Forms.Button btnFrm2;
+        private System.Windows.Forms.Button btnFinestreAperte;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menùToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem apriForm1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem apriForm2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem esciToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+    }
+}
+
---
+++ 02 - WindowsForms/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02___WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFrm1_Click(object sender, EventArgs e)
+        {
+            FormFiglia1 frm1 = new FormFiglia1();
+            frm1.MdiParent = this;
+            frm1.Text = "Figlia 1";
+            frm1.Size = new Size(210, 150);
+            frm1.StartPosition = FormStartPosition.Manual;
+            frm1.Location = new Point(10, 120);
+            frm1.Show();
+        }
+
+        private void btnFrm2_Click(object sender, EventArgs e)
+        {
+            FormFiglia2 frm1 = new FormFiglia2();
+            frm1.MdiParent = this;
+            frm1.Text = "Figlia 2";
+            frm1.Size = new Size(210, 150);
+            frm1.StartPosition = FormStartPosition.Manual;
+            frm1.Location = new Point(225, 120);
+            frm1.Show();
+        }
+
+        private void btnFinestreAperte_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sono state aperte " + this.MdiChildren.Length + " fineste figlie");
+            foreach (Form f in this.MdiChildren)
+            {
+                MessageBox.Show("Finestra '" + f.Text + "' aperta");
+            }
+        }
+
+        private void apriForm1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel.Text = "Premuto Apri Form 1";
+        }
+
+        private void apriForm2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel.Text = "Premuto Apri Form 2";
+        }
+
+        private void esciToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            FormEsci frmEsci = new FormEsci();
+            frmEsci.Text = "Esci?";
+
+            if (frmEsci.ShowDialog() == DialogResult.Cancel)
+                e.Cancel = true;
+        }
+    }
+}
